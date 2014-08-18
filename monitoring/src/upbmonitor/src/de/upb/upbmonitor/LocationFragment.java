e8e6a60c4ef05f0fbfbbcf72645137c3df9a566d
Manuel Peuster <manuel@peuster.de>
2014-08-18 15:05:10 +0200

    Storing manual location in shared preference

--- monitoring/src/upbmonitor/src/de/upb/upbmonitor/LocationFragment.java
+++ monitoring/src/upbmonitor/src/de/upb/upbmonitor/LocationFragment.java
@@ -1,7 +1,9 @@
 package de.upb.upbmonitor;
 
 import android.support.v4.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,9 +19,12 @@ public class LocationFragment extends Fragment
 {
 	private static final String LTAG = "LocationFragment";
 
+	private SharedPreferences settings;
 	private Switch switchManualLocation;
 	private NumberPicker npPositionX, npPositionY;
 	private TextView tvPH, tvPX, tvPY;
+
+	private int positionStepSize = 100;
 
 	/**
 	 * Returns a new instance of this fragment for the given section number.
@@ -41,6 +46,9 @@ public class LocationFragment extends Fragment
 		View rootView = inflater.inflate(R.layout.fragment_location, container,
 				false);
 
+		// get preference manager
+		settings = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
 		// get pointers to control elements
 		this.switchManualLocation = (Switch) rootView
 				.findViewById(R.id.switchManualLocation);
@@ -48,17 +56,21 @@ public class LocationFragment extends Fragment
 				.findViewById(R.id.npPositionX);
 		this.npPositionY = (NumberPicker) rootView
 				.findViewById(R.id.npPositionY);
-
 		this.tvPH = (TextView) rootView.findViewById(R.id.textViewPHeadline);
 		this.tvPX = (TextView) rootView.findViewById(R.id.textViewPX);
 		this.tvPY = (TextView) rootView.findViewById(R.id.textViewPY);
 
-		// TODO depended on property value
-		this.setEnableToPositionControls(false);
-
 		// fill number picker controls
-		this.fillNumberPicker(this.npPositionX, 0, 2000, 100);
-		this.fillNumberPicker(this.npPositionY, 0, 2000, 100);
+				this.fillNumberPicker(this.npPositionX, 0, 2000, positionStepSize);
+				this.fillNumberPicker(this.npPositionY, 0, 2000, positionStepSize);
+				
+		// load stored values
+		this.switchManualLocation.setChecked(this.getManualLocationPreference());
+		this.setEnableToPositionControls(this.getManualLocationPreference());
+		this.npPositionX.setValue(this.getPositionXPreference() / positionStepSize);
+		this.npPositionY.setValue(this.getPositionYPreference() / positionStepSize);
+
+		
 
 		// manual location switch listener
 		this.switchManualLocation
@@ -69,15 +81,40 @@ public class LocationFragment extends Fragment
 					public void onCheckedChanged(CompoundButton buttonView,
 							boolean isChecked)
 					{
+						// store value in shared preference
+						setManualLocationPreference(isChecked);
 						if (isChecked)
-						{
 							setEnableToPositionControls(true);
-							Log.i(LTAG, "Manual location enabled");
-						} else
-						{
+						else
 							setEnableToPositionControls(false);
-							Log.i(LTAG, "Manual location disabled");
-						}
+					}
+				});
+
+		// position change listener
+		this.npPositionX
+				.setOnValueChangedListener(new NumberPicker.OnValueChangeListener()
+				{
+					@Override
+					public void onValueChange(NumberPicker picker, int oldVal,
+							int newVal)
+					{
+						// store value in shared preference
+						if (newVal != oldVal)
+							setPositionXPreference(newVal * positionStepSize);
+					}
+				});
+
+		// position change listener
+		this.npPositionY
+				.setOnValueChangedListener(new NumberPicker.OnValueChangeListener()
+				{
+					@Override
+					public void onValueChange(NumberPicker picker, int oldVal,
+							int newVal)
+					{
+						// store value in shared preference
+						if (newVal != oldVal)
+							setPositionYPreference(newVal * positionStepSize);
 					}
 				});
 
@@ -104,6 +141,75 @@ public class LocationFragment extends Fragment
 		np.setMinValue(min);
 		np.setMaxValue(items - 1);
 		np.setValue(min);
+	}
+
+	private void setManualLocationPreference(boolean b)
+	{
+		if (settings == null)
+		{
+			Log.e(LTAG, "Could not access shared preferences.");
+			return;
+		}
+		SharedPreferences.Editor e = settings.edit();
+		e.putBoolean("pref_enable_manual_location", b);
+		e.commit();
+		Log.v(LTAG, "Changed manual location enaqbled to: " + b);
+	}
+
+	private boolean getManualLocationPreference()
+	{
+		if (settings == null)
+		{
+			Log.e(LTAG, "Could not access shared preferences.");
+			return false;
+		}
+		return settings.getBoolean("pref_enable_manual_location", false);
+	}
+
+	private void setPositionXPreference(int x)
+	{
+		if (settings == null)
+		{
+			Log.e(LTAG, "Could not access shared preferences.");
+			return;
+		}
+		SharedPreferences.Editor e = settings.edit();
+		e.putInt("pref_manual_location_x", x);
+		e.commit();
+		Log.v(LTAG, "Changed manual location X to: " + x);
+	}
+
+	private int getPositionXPreference()
+	{
+		if (settings == null)
+		{
+			Log.e(LTAG, "Could not access shared preferences.");
+			return 0;
+		}
+		return settings.getInt("pref_manual_location_x", 0);
+	}
+
+	private void setPositionYPreference(int y)
+	{
+		if (settings == null)
+		{
+			Log.e(LTAG, "Could not access shared preferences.");
+			return;
+		}
+		SharedPreferences.Editor e = settings.edit();
+		e.putInt("pref_manual_location_y", y);
+		e.commit();
+		Log.v(LTAG, "Changed manual location Y to: " + y);
+	}
+
+	private int getPositionYPreference()
+	{
+		if (settings == null)
+		{
+			Log.e(LTAG, "Could not access shared preferences.");
+			return 0;
+		}
+		return settings.getInt("pref_manual_location_y", 0);
 	}
 
 }
