32ca3ef7f2224ac6e7c35e5411412d5b59c0fb7f
Manuel Peuster <manuel@peuster.de>
2014-09-30 15:39:52 +0200

    Quick hack in UPB testbed AP driver to receive netstats. Not nice, but should work for firsts tests.

--- ap_manager/driver/upbtestbed.py
+++ ap_manager/driver/upbtestbed.py
@@ -57,12 +57,59 @@ class ApDriver():
         return "radio_on"
 
     def get_network_stats(self, ap):
+        """
+        Fetches network stats from access point.
+        Done by executing and greping ifconfig via ssh.
+        TODO: Find a better solution. This is just a quick hack.
+        """
         logging.info("[ApDriver] Getting networks stats for %s" % ap)
+        # get ssh shortcut name for AP
+        ssh_name = ap.driver_info["ssh_name"]
+        # get information about rx/tx bytes from remote ifconfig
+        proc = subprocess.Popen(
+            "ssh -n %s 'ifconfig wlan0 | grep bytes'" % ssh_name,
+            shell=True,
+            stdout=subprocess.PIPE)
+        # parse results
+        new_stats = self.__parse_ifconfig_output(proc.stdout.readline())
+        # build return dict
+        rxb = 0
+        txb = 0
+        if "rx_bytes" in new_stats:
+            rxb = new_stats["rx_bytes"]
+        if "tx_bytes" in new_stats:
+            txb = new_stats["tx_bytes"]
+
         return {"aps": [
-                {"name": "vap0",
-                 "rxbyte": 1234,
-                 "rxpkt": 123,
-                 "txbyte": 6789,
-                 "txpkt": 678,
+                {"name": "wlan0",
+                 "rxbyte": rxb,
+                 "rxpkt": 0,  # packets are not counted by this driver
+                 "txbyte": txb,
+                 "txpkt": 0,  # packets are not counted by this driver
                  "timestamp": time.time()
                  }]}
+
+    def __parse_ifconfig_output(self, line):
+        """
+        Parses ifconfig output line to receive current bytecount.
+        """
+        if line is None or len(line) < 10:
+            return {}
+        rxb = 0
+        txb = 0
+        parts = line.split(" ")
+        found = 0
+        try:
+            for p in parts:
+                if "bytes:" in p:
+                    subparts = p.split(":")
+                    if found < 1:
+                        rxb = int(subparts[1])
+                        found += 1
+                    else:
+                        txb = int(subparts[1])
+        except:
+            rxb = 0
+            txb = 0
+
+        return {"rx_bytes": rxb, "tx_bytes": txb}
